996a39aca7e67dc0f8e5173465ffdc564d1f6b5f
ZuzanaMatiaskova <zuzkamatiaskova@gmail.com>
2023-02-28 19:45:57 +0100

    Barevne palety vcetne obou komponent

--- src/index.jsx
+++ src/index.jsx
@@ -4,26 +4,6 @@ import './style.css';
 import { palettes } from "./palettes.js"
 import Palette from "./Palette/Palette.jsx"
 
-/*
-<div className="palette">
-          <div className="palette-scheme palette-scheme--horizontal">
-            <img className="scheme-image" src="/img/ocean-waves.jpg" alt="Ocean Waves" />
-            <div className="scheme-colors">
-              {palettes[1].colors.map((item) => (
-                <SchemeColor
-                  key={item}
-                  color={item} />))}
-            </div>
-          </div>
-          <div className="palette-info">
-            <h2>Ocean Waves</h2>
-            <p>Wildlife has given humanity an indescribable variety of color shades that, at first glance, cannot be combined for balance. However, the combination of dark greens canonically turns into a bright light green shade, growing into banana yellow. From the yellow tones, a sudden rich pink breaks through.</p>
-
-            <p>Photo by <a href="https://unsplash.com/photos/wc9avd2RaN0" target="_blank">Christoffer Engström</a>.</p>
-          </div>
-        </div>
-  */
-
 const App = () => {
   return (
     <div className="container">
